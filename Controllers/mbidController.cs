1dcbaf49c8e43883359556518630d0eb32103435
Bjorn Adlerborn <bjorn.adlerborn@gmail.com>
2020-11-05 08:48:34 +0100

    First code

---
+++ Controllers/mbidController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using musicbrainz.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace musicbrainz.Controllers
+{
+    [Route("api/MbidController")]
+    [ApiController]
+    public class MbidController : ControllerBase
+    {
+        // http://musicbrainz.org/ws/2/artist/5b11f4ce-a62d-471e-81fc-a69a8278c7da?&fmt=json&inc=url-rels+release-groups
+        public MbidController()
+        {       
+            
+        }
+
+        // GET api/MbidC/??
+        [HttpGet("{id}")]
+        public object Get(string id)
+        {
+            string responseBody = "Not Found";
+            
+            try {
+
+
+                MBIdRequest MBIdR = new MBIdRequest();
+                MBIdR.DoSearch(id);
+
+            }
+            catch (Exception err)
+            {
+
+
+            }
+            return responseBody;
+        }
+
+
+    }
+}
---
+++ Models/MBIdRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace musicbrainz.Models
+{    
+    public class MBIdRequest
+    {
+        private const string BaseUrl = "http://musicbrainz.org/ws/2/artist/";
+        private const string FilterQuery = "?&fmt=json&inc=url-rels+release-groups";
+        public bool DoSearch(string MBId)
+        {
+            if (MBId == null || MBId == "")
+            {
+                throw new Exception("MBId not set");
+            }
+            WebClient WC = new WebClient();            
+            WC.Headers.Add("User-Agent", "C# App");
+            Stream data = WC.OpenRead(BaseUrl + MBId + FilterQuery);
+            StreamReader reader = new StreamReader(data);
+            string jsonData = reader.ReadToEnd();
+            var deserializedJson = JsonConvert.DeserializeObject<Dictionary<object, object>>(jsonData);
+            var relationsJson = deserializedJson["relations"].ToString();
+            int pos1 = relationsJson.IndexOf("\"type\": \"wikidata\"");
+            //"\"type\":\"wikidata\"
+            /*
+            var details = JObject.Parse(deserializedJson.ToString());
+            var R = details.Root;
+            var details2 = JObject.Parse(R.ToString()); 
+            */
+            return true;
+        }
+
+
+    }
+}
